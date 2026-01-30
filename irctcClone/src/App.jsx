d46c03220e54a726a9b2f7411a4ba44c67a944e5
ranjith <ranjithkankati2@gmail.com>
2026-01-30 22:18:57 +0530

    initial page started

--- irctcClone/src/App.jsx
+++ irctcClone/src/App.jsx
@@ -1,29 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import leftlogo from './assets/leftlogo.png'
+import rightlogo from './assets/rightlogo.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className='header'>
+      <div className='header-left'>
+        <img src={leftlogo.png} alt='leftlogo'></img>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
+      <div className='header-centre'>
+
       </div>
-      
+      <div className='header-right'>
+
+      </div>
+     </div>
+     <div className='login-card'>
+
+     </div>
+     <div className='services'>
+
+     </div>
+     <div className='holidays'>
+
+     </div>
+     <div className='footer'>
+
+     </div>
     </>
   )
 }
